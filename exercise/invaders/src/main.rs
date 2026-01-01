9b9e245ec193f5887b2e031241de2286af5c999c
Min Hsu <minhsu.dev@gmail.com>
2026-01-01 19:34:04 +0800

    feat: shotting

--- exercise/invaders/src/main.rs
+++ exercise/invaders/src/main.rs
@@ -9,7 +9,7 @@ use invaders::render;
 use rusty_audio::Audio;
 use std::error::Error;
 use std::sync::mpsc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::{io, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -48,8 +48,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Game loop
     let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
         // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
 
         // Input
@@ -58,6 +61,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 match key.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -67,6 +75,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Update
+        player.update(delta);
 
         // Draw & Render
         player.draw(&mut curr_frame);
