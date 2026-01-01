94d4679a00eced59fb0aded32e75e4d91810acbd
Min Hsu <minhsu.dev@gmail.com>
2026-01-01 18:53:55 +0800

    feat: rendering and multithreading

--- exercise/invaders/src/main.rs
+++ exercise/invaders/src/main.rs
@@ -1,8 +1,15 @@
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{self, Event, KeyCode, read};
 use crossterm::terminal::EnterAlternateScreen;
-use crossterm::{terminal, ExecutableCommand};
+use crossterm::terminal::LeaveAlternateScreen;
+use crossterm::{ExecutableCommand, terminal};
+use invaders::frame::{self, new_frame};
+use invaders::render;
 use rusty_audio::Audio;
 use std::error::Error;
-use std::io;
+use std::sync::mpsc;
+use std::time::Duration;
+use std::{io, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -17,9 +24,62 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Terminal
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
-    stdout.execute(EnterAlternateScreen);
+    // open alternate screen and hide current behind scene
+    stdout.execute(EnterAlternateScreen)?;
+    // hide cursor
+    stdout.execute(Hide)?;
 
-    // clean up
+    // Render loop in separate thread
+    let (render_tx, render_rx) = mpsc::channel::<Vec<Vec<&str>>>();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame
+        }
+    });
+
+    // Game loop
+    'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key) = read()? {
+                match key.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    // default case is must be handled
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw & Render
+        render_tx.send(curr_frame)?;
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+
     audio.wait();
+    // show cursor
+    stdout.execute(Show)?;
+    // close alternate screen
+    stdout.execute(LeaveAlternateScreen)?;
+
+    terminal::disable_raw_mode()?;
+
     Ok(())
 }
